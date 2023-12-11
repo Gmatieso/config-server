bbbe5119bb9c620aaf12bf3ffcca2301988a18b9
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-12-11 23:48:24 +0300

    Added new files

---
+++ cloud-config-server/src/test/java/com/techg/cloud/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techg.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
